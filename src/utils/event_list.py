5bdf71a4cedcf7fcdef0489f01d9ea4801cab502
fkuhnert <fkuhnert@dcc.ufrj.br>
2022-07-29 19:10:16 -0300

    Impleted event list

---
+++ src/utils/event_list.py
@@ -0,0 +1,88 @@
+class Event:
+    def __init__(self, time, type):
+        self.time = time
+        self.event_type = type
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class Event_list:
+    '''Lista duplamente encadeada, mantém a lista de eventos em ordem crescente'''
+
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    # Insere eventos mantendo ordem crescente de tempo
+    def insert_event(self, NewEvent: Event):
+        self.length = self.length + 1
+        NewNode = Node(NewEvent)
+
+        if self.head is None:
+            self.head = NewNode
+            return
+
+        node = self.head
+        last = None
+        while node is not None:
+            if node.data.time > NewEvent.time:
+                NewNode.next = node
+                if node.prev is not None:
+                    node.prev.next = NewNode
+                    NewNode.prev = node.prev
+                else:
+                    self.head = NewNode
+                node.prev = NewNode
+                return
+            last = node
+            node = node.next
+
+        NewNode.prev = last
+        last.next = NewNode
+        return
+
+    def get_first(self) -> Event:
+        return self.head.data
+
+    def pop(self) -> Event:
+        self.length = self.length - 1
+        val = self.head.data
+        next = self.head.next
+
+        del(self.head)
+        self.head = next
+
+        return val
+
+    # Printa todos os elementos da lista
+    def print_all(self):
+        node = self.head
+        while (node is not None):
+            print(f"{node.data.time} - {node.data.event_type}")
+            node = node.next
+
+
+def exemplo():
+    eventos = Event_list()
+    eventos.insert_event(Event(4, "um"))
+    eventos.insert_event(Event(5, "um"))
+    eventos.insert_event(Event(2, "um"))
+    eventos.insert_event(Event(1, "um"))
+    eventos.insert_event(Event(6, "um"))
+    eventos.insert_event(Event(7, "dois"))
+    eventos.insert_event(Event(1.8, "dois"))
+
+    eventos.print_all()
+    print()
+
+    while eventos.length > 0:
+        print("Pop: ", eventos.pop().time)
+
+
+if __name__ == "__main__":
+    exemplo()
--- src/utils/random_generator.py
+++ src/utils/random_generator.py
@@ -2,24 +2,29 @@ from random import random
 from random import seed as set_seed
 import math
 
+# Classe base para o gerador de números aleatórios
 class RandomGenerator:
-    def __init__(self, seed, rate):
+    def __init__(self, seed: int, rate: float):
         set_seed(seed)
         self.rate = rate
 
-    def next(self):
+    def next(self) -> float:
         pass
 
+# Gera números aleatórios entre 0 e 1 normalmente
 class Regular(RandomGenerator):
-    def next(self):
+    def next(self) -> float:
         return random()
 
+# Gera números aleatórios com distribuição exponencial
+# de acordo com a fórmula ln(1-rand)/-λ
 class Exponential(RandomGenerator):
-    def next(self):
+    def next(self) -> float:
         return math.log(1-random())/-self.rate
 
+# Números não aleatórios gerados deterministicamente
 class Deterministic(RandomGenerator):
-    def next(self):
+    def next(self) -> float:
         return 1/self.rate
 
 def main():
